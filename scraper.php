17dd0d009aaed655aa2505ce4a8c856a8634828b
luudanh <luudanhdanh@hotmail.ca>
2014-06-02 20:23:41 +0700

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -30,7 +30,7 @@ $noidung = $trr->innertext;
 //$noidung = utf8_encode($noidung);
 if(mb_strlen($noidung) >1000){
     $j++;
-  scraperwiki::save_sqlite(array('id'),array('id'=> $j.$val->id, 'title'=>$val->title,'url'=> $val->url,'content'=>base64_encode($noidung),'order'=> $j,'num'=>$val->num,'reply'=>$val->reply,,'type'=>$val->type));
+  scraperwiki::save_sqlite(array('id'),array('id'=> $j.$val->id, 'title'=>$val->title,'url'=> $val->url,'content'=>base64_encode($noidung),'order'=> $j,'num'=>$val->num,'reply'=>$val->reply,'type'=>$val->type));
 }
    
 }
